3a7cf1d7662fc9cb9e5d3632775fa2593545a05d
PassiG <PassiG@users.noreply.github.com>
2022-05-02 12:59:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from urllib.error import URLError
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.header('Breakfast Favorites2')
+streamlit.heade('Breakfast Favorites2')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
