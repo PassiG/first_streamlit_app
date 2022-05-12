fd2a006f610263f4fd132c769dd9a57a8642f8c6
PassiG <PassiG@users.noreply.github.com>
2022-05-12 11:44:03 +0200

    Update streamlit_app.py
    change title

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal2')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
